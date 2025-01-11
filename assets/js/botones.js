a58b694074047f9a5575af014ffd5238ea0804b6
pamep <pamelaportillocabrera@gmail.com>
2025-01-11 01:00:18 -0300

    textos y metimos un tooltip

--- assets/js/botones.js
+++ assets/js/botones.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtener los botones
+    
     var btnIngles = document.getElementById('btn-ingles');
     var btnEspanol = document.getElementById('btn-espanol');
 
-    // Función para cambiar a inglés
+    
     btnIngles.addEventListener('click', function() {
         window.location.href = "index_en.html";
     });
 
-    // Función para cambiar a español
+   
     btnEspanol.addEventListener('click', function() {
         window.location.href = "index.html";
     });
---
+++ assets/js/tooltip.js
@@ -0,0 +1,27 @@
+console.log('El script se ha cargado correctamente.');
+
+const elements = document.querySelectorAll('[data-tooltip]');
+
+elements.forEach(element => {
+  const tooltipText = element.dataset.tooltip;
+  console.log(`Tooltip para ${element}: ${tooltipText}`);  // Verifica que los tooltips se estén leyendo correctamente
+  const tooltipElement = document.createElement('div');
+  tooltipElement.classList.add('tooltip');
+  tooltipElement.textContent = tooltipText;
+
+  element.addEventListener('mouseover', () => {
+    console.log('Mouse sobre el elemento, mostrando tooltip.');
+    document.body.appendChild(tooltipElement);
+    tooltipElement.style.visibility = 'visible';
+    const rect = element.getBoundingClientRect();
+    tooltipElement.style.position = 'absolute';
+    tooltipElement.style.top = `${rect.top + window.scrollY - tooltipElement.offsetHeight - 5}px`;
+    tooltipElement.style.left = `${rect.left + (rect.width - tooltipElement.offsetWidth) / 2}px`;
+  });
+
+  element.addEventListener('mouseout', () => {
+    console.log('Mouse fuera del elemento, ocultando tooltip.');
+    tooltipElement.style.visibility = 'hidden';
+    document.body.removeChild(tooltipElement);
+  });
+});
